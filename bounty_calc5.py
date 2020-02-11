167dd4ee0bfedcea7bd00cdbed1f67fc39293fc2
nomadstrategy <nomadstrategy@posteo.co>
2020-02-11 22:59:21 +0700

    introduce buggy pot odds caluclator

--- bounty_calc5.py
+++ bounty_calc5.py
@@ -19,13 +19,16 @@ mtt.convert_bounty(9.8) --> converts $9.8 displayed bounty to chipvalue
 class BountyTournament:
     """
     generic bounty tournament structure
+    adjusting values below sets defaults
     """
 
     buyin: int = 10
     starting_stack: int = 10000
     starting_knockout: float = 4.9
     bounty_value = 0.4
-    starting_bounty_chipvalue = starting_stack * bounty_value
+    starting_bounty_chipvalue = (starting_stack * bounty_value) / (
+        buyin / starting_knockout
+    )
     current_conversion = None
 
     def convert_bounty(self, bounty=None):
@@ -42,6 +45,9 @@ class BountyTournament:
         print(f"${bounty} converts to --> {self.current_conversion} chips")
         return self.current_conversion
 
+    def pot_odds(self):
+        pass
+
     def __str__(self):
         return f"""
         ___________________________________________________________________
@@ -52,6 +58,41 @@ class BountyTournament:
         ____________________________________________________________________"""
 
 
-mtt = BountyTournament(buyin=20, starting_stack=10000, starting_knockout=9.8)
-mtt.convert_bounty(33)
-print(mtt.current_conversion)
+@dataclass
+class ProgressiveSuperKnockout(BountyTournament):
+    buyin: float = 33
+    starting_stack: int = 3000
+    starting_knockout: float = buyin / 2
+
+
+@dataclass
+class SuperKnockout(BountyTournament):
+    buyin: float = 10
+    starting_stack: int = 10000
+    starting_knockout = buyin / 2
+    bounty_value = 0.5
+    starting_bounty_chipvalue = starting_stack
+
+
+@dataclass
+class FlatKnockout(BountyTournament):
+    buyin: float = 10
+    starting_stack: int = 10000
+
+
+def pot_odds(mtt_instance, current_pot=None, price_to_call=None):
+    logging.debug(mtt_instance)
+    if not current_pot:
+        current_pot = int(input("Enter current potsize:  "))
+
+    if not price_to_call:
+        price_to_call = int(input("Enter cost to call:  "))
+
+    final_pot = current_pot + price_to_call  + mtt_instance.convert_bounty()
+    pot_odds = price_to_call / final_pot * 100
+    return f"pot odds: {round(pot_odds)}%"
+
+
+a = SuperKnockout()
+
+print(pot_odds(a))
