509226c792caa2a024b77147a267b0379af6e2fc
nomadstrategy <nomadstrategy@posteo.co>
2020-02-12 21:56:01 +0700

    basic bounty_buddy implementation

--- bounty_buddy.py
+++ bounty_buddy.py
@@ -4,20 +4,77 @@ from dataclasses import dataclass
 logging.basicConfig(level=logging.DEBUG)
 
 
-class KnockoutTournament:
+"""
+Generic KO tournaments easily generated
+example:
+mtt = BountyTournament(buyin=10, starting_stack=10000, starting_knockout=4.9)
+(Default settings)
+
+mtt.convert_bounty() --> prompts for displayed opponent bounty, converts
+mtt.convert_bounty(9.8) --> converts $9.8 displayed bounty to chipvalue
+
+start 10k chips
+starting bounty: 22
+10k * 40% = 4k
+4k per starting bounty
+"""
+
+
+class BountyTournament:
     """
-    basic tournament bones
+    generic bounty tournament structure
+    adjusting values below sets defaults
     """
 
-    BOUNTY_VALUE = 0.4
+    def __init__(self, starting_stack, starting_bounty, bounty_rate=0.4):
+        self.starting_stack = starting_stack
+        self.starting_bounty = starting_bounty
+        self.bounty_rate = bounty_rate
+        self.initial_bounty_value = round(self.starting_stack * self.bounty_rate)
 
-    def __init__(self, buyin, starting_stack, _format=None):
-        self.buyin = float(buyin)
-        self.starting_stack = int(starting_stack)
-        self._format = _format
+    def convert_bounty(self, displayed_bounty=None):
+        if not displayed_bounty:
+            displayed_bounty = float(input("Enter the displayed bounty... $"))
+        bounty_value = self.initial_bounty_value * (
+            displayed_bounty / self.starting_bounty
+        )
+        print(
+            f"{self}\ncurrent bounty ${displayed_bounty} is worth \
+            {bounty_value} chips"
+        )
+        return bounty_value
+
+    def __str__(self):
+        return f"""
+        ___________________________________________________________________
+        Bounty Tournament starting with:
+        {self.starting_stack} chips/ ${self.starting_bounty} KO -->
+        {self.initial_bounty_value} initial bounty chipvalue
+        ____________________________________________________________________"""
 
 
-mtt = KnockoutTournament(10, 3000, "SKO")
+my_mtt = BountyTournament(10000, 22)
+# print(my_mtt.initial_bounty_value)
+# print(my_mtt)
 
-print(mtt)
+# my_mtt.convert_bounty(44)
 
+
+def get_pot_odds(tournament=None):
+    """
+    Enter a tournament to add bounty chips to pot when opponent at risk.
+    """
+    current_pot = int(input("Enter current pot size: "))
+    facing_bet = int(input("Enter the cost to call: "))
+    total_pot = current_pot + facing_bet
+    if tournament:
+        total_pot += tournament.convert_bounty()
+
+    logging.debug(f"cost: {facing_bet}, total pot: {total_pot}")
+    pot_odds = (facing_bet / total_pot) * 100
+    logging.debug(pot_odds)
+    print(f"{round(pot_odds)}% equity needed to call")
+    return pot_odds
+
+
+print(get_pot_odds(my_mtt))
--- test_bounty.py
+++ test_bounty.py
@@ -1,17 +1,63 @@
-from bounty_buddy import Tournament, KnockoutTournament
+from bounty_buddy import BountyTournament
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def test_creating_mtt():
-    my_mtt = KnockoutTournament(10, 5000)
+    my_mtt = BountyTournament(10, 5000)
     assert my_mtt.buyin == 10
     assert my_mtt.starting_stack == 5000
-    my_mtt = KnockoutTournament(333, 10000)
+    my_mtt = BountyTournament(333, 10000)
     assert my_mtt.buyin == 333
     assert my_mtt.starting_stack == 10000
 
 
-def test_creating_new_structure_mtt():
-    my_mtt = Tournament(bounty_value=0.75, name="SKO", progressive=True)
-    assert my_mtt.bounty_value == 0.75
-    assert my_mtt.name == "SKO"
-    assert my_mtt.progressive == True
+def test_conversions():
+    my_mtt = BountyTournament(10, 10000)
+    assert my_mtt.starting_bounty_chipvalue == (
+        my_mtt.bounty_value * my_mtt.starting_stack
+    )
+    assert (
+        my_mtt.convert_bounty(10)
+        == (10 / my_mtt.starting_knockout) * my_mtt.starting_bounty_chipvalue
+    )
+
+
+"""
+# def test_creating_new_structure_mtt():
+#     my_mtt = BountyTournament(bounty_value=0.75, name="SKO", progressive=True)
+#     assert my_mtt.bounty_value == 0.75
+#     assert my_mtt.name == "SKO"
+#     assert my_mtt.progressive == True
+
+
+
+test calculations and features of the bounty calculator and use cases
+
+User Story: 
+user should be able to call tournaments as easily as possible.
+
+#1. Users should be able to create a custom tournament
+    *factory function for classes?
+    # buyin, starting stack different for each tournament
+    # knockout type, value might be consistent between all types 
+    # tournament = knockout(bounty_value=0.4, starting_bounty=0.5, bounty_earned=0.5)
+
+#2. Users should be able to easily get bounty conversions
+
+    #   $10 Bounty Tournament starting with:
+        10000 chips/ $4.9 KO -->
+        10000 initial bounty chipvalue
+        ... enter new bounty to get an updated chipvalue (or q, h, n): --> ...
+    #
+
+#3. Users should be able to continue working on a tournament until finished
+    # while True... break on 'q'
+
+#4  easy to understand syntax. 
+# mtt = pko(10,3000) --> progressive knockout with 10 bi, 3000 starting chips
+# mtt.calc(30) --> displays pko(10,3000).calc(30) or as needed (30 displayed bounty)
+
+#5 pot-odds including adjusted bounty chipvalue
+"""
